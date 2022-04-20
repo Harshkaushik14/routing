b4d755051b6e3deb840fe05d3a4d7b5bb59d6573
Harshkaushik14 <kaushikharsh86@gmail.com>
2022-04-20 11:21:48 +0530

    common routing tried

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FirstComponent } from './first/first.component';
 import { SecondComponent } from './second/second.component';
+import { ThirdComponent } from './third/third.component';
 
 
 
 const routes: Routes = [
   { path: 'first-component', component: FirstComponent },
   { path: 'second-component', component: SecondComponent },
+  { path: 'third-component', component: ThirdComponent },
 ];
 
 @NgModule({
